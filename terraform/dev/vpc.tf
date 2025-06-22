748f485245dddf3e45c3cb59877c34658bb980e7
foo-543674 <foo.avoutar7654@gmail.com>
2025-06-22 12:29:42 +0000

    [feat]add dev env

---
+++ terraform/dev/vpc.tf
@@ -0,0 +1,14 @@
+module "vpc" {
+  source = "../modules/vpc"
+
+  name                = var.project_name
+  environment         = var.environment
+  vpc_cidr            = var.vpc_cidr
+  subnets             = [
+    for subnet in merge(var.subnets) : {
+      az           = subnet.az
+      private_cidr = subnet.private_cidr
+      public_cidr  = subnet.public_cidr
+    }
+  ]
+}
